33332a8cae177cb4cefbbf6b51d734ec72c28400
TimeFish <107293965+TimeFish-yu@users.noreply.github.com>
2026-03-06 00:48:06 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/share_1771773414036.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Zane",
 	bio: "I'm a developer, student, designer.",
 	links: [
